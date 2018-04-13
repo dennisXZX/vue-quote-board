2c5719aa98a038af12c8f8b5af0fb49a5cc1ccbf
Dennis Xiao <dennisboys@gmail.com>
2018-04-13 21:52:13 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Vue Quote Board
+
+Powered by Vue
+
+#### Steps
+- run `npm install` to install dependencies
+- run `npm run dev` to run the app server
+- default port: `localhost:8080`
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></Header>
+        <NewQuote @quoteAdded="quoteAdded"></NewQuote>
+        <QuoteGrid :quotes="quotes" @quoteDeleted="quoteDeleted"></QuoteGrid>
+        <InfoBar></InfoBar>
+    </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import InfoBar from './components/InfoBar.vue';
+
+  export default {
+    components: {
+      Header,
+      QuoteGrid,
+      NewQuote,
+      InfoBar
+    },
+    data: function () {
+      return {
+        quotes: [
+          'Just a quote to see',
+          'test quote',
+          'Just a quote to see',
+          'test quote',
+        ],
+        maxQuotes: 5
+      }
+    },
+    methods: {
+      quoteAdded(newQuote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete quote first.');
+        }
+        this.quotes.unshift(newQuote);
+      },
+      quoteDeleted(index) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
